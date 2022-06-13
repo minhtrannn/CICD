e1b43583243586497a92ea7bab48c4ca7b5c837e
minhtn4 <minhtn@topcv.vn>
2022-06-13 22:25:32 +0700

    mt update

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations! HELLO THIS IS MINH</h1>
+        <h1 class="display-4">Congratulations! HELLO THIS IS MINH TRAN DZ 123</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
